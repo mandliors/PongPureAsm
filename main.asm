0dcd00d7fc0d19c158d3ff0695cfc8c4c6f654db
Mándli Örs <mandliors@gmail.com>
2024-12-10 03:22:44 +0100

    racket movement

--- main.asm
+++ main.asm
@@ -10,26 +10,42 @@
 ; window constants
 SW_SHOW				equ 5
 
+PM_REMOVE			equ 0x0001
+
 WM_CREATE			equ 0x0001
 WM_DESTROY			equ 0x0002
 WM_PAINT			equ 0x000F
 WM_KEYDOWN			equ 0x0100
 WM_KEYUP			equ 0x0101
 WM_CLOSE			equ 0x0010
+WM_QUIT				equ 0x0012
 
+
+; data
+section .data use32
+	wKeyDown				dd 0
+	sKeyDown				dd 0
+	upArrowDown				dd 0
+	downArrowDown			dd 0
+
+	racket1Pos				dd 300
+	racket2Pos				dd 300
 
 ; read-only data
 section .rodata use32
+	windowWidth				dd 800
+	windowHeight			dd 600
+
 	windowClassName         db "door", 0
 	windowName				db "nigus bigus", 0
 	
+	racketWidth				dd 10
+	racketHeight			dd 100
+	racketOffset			dd 30
+	racketSpeed				dd 3
+
 	sigma 					db "the negus ruled Ethiopia until the coup of 1974", 0
 	sigma_length 			equ $-sigma
-
-	wKeyDown				db 0
-	sKeyDown				db 0
-	upArrowDown				db 0
-	downArrowDown			db 0
 	
 ; uninitialized static variables
 section .bss use32
@@ -43,14 +59,18 @@ section .bss use32
 ; text segment (or code segment) 
 section .text use32 
 	; library functions use __stdcall calling convention
-    dll_import    user32.dll,   GetMessageA
+    dll_import    user32.dll,   PeekMessageA
     dll_import    user32.dll,   TranslateMessage
     dll_import    user32.dll,   DispatchMessageA
     dll_import    user32.dll,   DefWindowProcA
     dll_import    user32.dll,   ShowWindow
     dll_import    user32.dll,   UpdateWindow
+    dll_import    user32.dll,	PostMessageA
     dll_import    user32.dll,	PostQuitMessage
     dll_import    user32.dll,	DestroyWindow
+
+	dll_import	  user32.dll, 	GetDC
+	dll_import 	  user32.dll, 	ReleaseDC
 
     dll_import    user32.dll,	BeginPaint
     dll_import    user32.dll,	EndPaint
@@ -62,6 +82,7 @@ section .text use32
 	dll_import    gdi32.dll,	CreateSolidBrush
 	dll_import    gdi32.dll,	DeleteObject
 
+    dll_import    kernel32.dll, Sleep
     dll_import    kernel32.dll, ExitProcess
 
 	extern		  init
@@ -84,8 +105,8 @@ section .text use32
 	; create the window
 	push	windowClassName			; window class name (previously created)
 	push	windowName				; window name
-	push 	600						; height
-	push 	800						; width
+	push 	dword [windowHeight]    ; height
+	push 	dword [windowWidth]     ; width
 	push 	windowHandle			; windows handle
 	call 	create_window
 	add		esp, 20
@@ -96,25 +117,162 @@ section .text use32
 	call	print_to_stdout
 	add		esp, 8
 
-; main loop to poll events
+; main loop
 mainMessageLoop:
+	; pop a message
+	push	PM_REMOVE			; remove the message from the queue
 	push	0
 	push	0
 	push	0
 	push	messageBuffer
-	call	[GetMessageA]
-	cmp		eax, 0
-	jle		quitProgram
+	call	[PeekMessageA]
 
+	cmp		eax, 0				; no message left
+	je		_messageLoopEnd
+
+	; for some reason, for me the WM_QUIT disappears into the ether (so I need to handle it here)
+	cmp		dword [messageBuffer+4], WM_QUIT
+	je		_onQuit
+
+	; handle the message normally
 	push	messageBuffer
 	call	[TranslateMessage]
-	
+
 	push	messageBuffer
 	call	[DispatchMessageA]
 
-	jmp		mainMessageLoop
+	jmp 	mainMessageLoop
 
+_messageLoopEnd:
+	call	gameLoop
+
+	push	16					; approximately 60 FPS (1s / 16ms ~= 60)
+	call	[Sleep]
+
+	jmp		mainMessageLoop
 	
+	
+gameLoop:
+	push	ebp
+	mov		ebp, esp
+
+	push 	ebx
+	push	esi
+
+_update:
+	cmp		dword [wKeyDown], 1
+	jne		_update_NotW
+	mov		eax, dword [racket1Pos]
+	sub		eax, dword [racketSpeed]
+	mov 	dword [racket1Pos], eax
+_update_NotW:
+	cmp		dword [sKeyDown], 1
+	jne		_update_NotS
+	mov		eax, dword [racket1Pos]
+	add		eax, dword [racketSpeed]
+	mov 	dword [racket1Pos], eax
+_update_NotS:
+	cmp		dword [upArrowDown], 1
+	jne		_update_NotUp
+	mov		eax, dword [racket2Pos]
+	sub		eax, dword [racketSpeed]
+	mov 	dword [racket2Pos], eax
+_update_NotUp:
+	cmp		dword [downArrowDown], 1
+	jne		_update_NotDown
+	mov		eax, dword [racket2Pos]
+	add		eax, dword [racketSpeed]
+	mov 	dword [racket2Pos], eax
+_update_NotDown:
+	; update ball
+
+_render:
+	; hdc = GetDC(hwnd)
+	push	dword [windowHandle]
+	call	[GetDC]
+	mov		ebx, eax
+
+	sub		esp, 16				 ; rect
+	
+	; background 
+	push	esp					 ; &rect
+	push	dword [windowHandle] ; windowHandle
+	call 	[GetClientRect]
+
+	lea		ecx, [esp]
+	push 	dword [blackBrush]	 ; HBRUSH
+	push	ecx					 ; &rect
+	push	ebx					 ; hdc
+	call	[FillRect]
+
+	; racket offset
+	mov		eax, dword [racketHeight]
+	mov		edx, 2
+	div 	dl							; ax := al / dl
+	movzx	esi, ax						; offset in esi
+
+	; player 1
+	mov		ecx, dword [racket1Pos]
+	
+	mov		eax, dword [racketOffset]
+	mov		dword [esp +0], eax
+	
+	mov		eax, ecx
+	sub		eax, esi
+	mov		dword [esp +4], eax
+	
+	mov		eax, dword [racketOffset]
+	add		eax, dword [racketWidth]
+	mov		dword [esp +8], eax
+	
+	mov		eax, ecx
+	add		eax, esi
+	mov		dword [esp+12], eax
+
+	lea		ecx, [esp]
+	push 	dword [whiteBrush]	 ; HBRUSH
+	push	ecx					 ; &rect
+	push	ebx					 ; hdc
+	call	[FillRect]
+
+	; player 2
+	mov		ecx, dword [racket2Pos]
+
+	mov		eax, dword [windowWidth]
+	sub		eax, dword [racketOffset]
+	sub		eax, dword [racketWidth]
+	mov		dword [esp +0], eax
+	
+	mov		eax, ecx
+	sub		eax, esi
+	mov		dword [esp +4], eax
+
+	mov		eax, dword [windowWidth]
+	sub		eax, dword [racketOffset]
+	mov		dword [esp +8], eax
+
+	mov		eax, ecx
+	add		eax, esi
+	mov		dword [esp+12], eax
+
+	lea		ecx, [esp]
+	push 	dword [whiteBrush]	 ; HBRUSH
+	push	ecx					 ; &rect
+	push	ebx					 ; hdc
+	call	[FillRect]
+
+	; ReleaseDC(hwnd, hdc) [hwnd is already on the stack]
+	push	ebx
+	push 	dword [windowHandle]
+	call	[ReleaseDC]
+
+	pop 	esi
+	pop 	ebx
+
+	mov		esp, ebp
+	pop		ebp
+	ret
+
 ; main window procedure
 windowProcedure:
 	push	ebp
@@ -138,6 +296,8 @@ windowProcedure:
 	je		_onKeyUp
 	cmp		dword [ebp_message], WM_CLOSE
 	je		_onClose
+	cmp		dword [ebp_message], WM_QUIT
+	je		_onQuit
 	
 _defaultProcedure:
 	push	dword [ebp_lparam]
@@ -179,43 +339,14 @@ _onDestroy:
 	pop		ebp
 	ret		16
 
-_onPaint:
-	push 	ebx
-	push	esi
+_onPaint: ; empty paint code
+	sub		esp, 64 				; paintStruct
 
 	; BeginPaint(windowHandle, &paintStruct)
-	sub		esp, 64 				; paintStruct
 	push	esp						; &paintStruct
 	push	dword [windowHandle]	; windowHandle
 	call	[BeginPaint]
 	mov		ebx, eax				; save hdc
-
-
-	sub		esp, 16				 ; rect
-	
-	; background 
-	push	esp					 ; &rect
-	push	dword [windowHandle] ; windowHandle
-	call 	[GetClientRect]
-
-	lea		ecx, [esp]
-	push 	dword [blackBrush]	 ; HBRUSH
-	push	ecx					 ; &rect
-	push	ebx					 ; hdc
-	call	[FillRect]
-
-	; player 1
-	mov		dword [esp +0], 30
-	mov		dword [esp +4], 250
-	mov		dword [esp +8], 40
-	mov		dword [esp+12], 350
-
-	lea		ecx, [esp]
-	push 	dword [whiteBrush]	 ; HBRUSH
-	push	ecx					 ; &rect
-	push	ebx					 ; hdc
-	call	[FillRect]
-
 
 	; EndPaint(windowHandle, &paintStruct)
 	lea		eax, [esp+16]
@@ -223,53 +354,48 @@ _onPaint:
 	push	dword [windowHandle]	; windowHandle
 	call	[EndPaint]
 
-	add		esp, 16
-	add		esp, 64
-	pop		esi
-	pop 	ebx
-
 	mov		esp, ebp
 	pop		ebp
 	ret		16
 
 _onKeyDown:
 	cmp		dword [ebp_wparam], 0x57	; W key
-	jne		_notWKeyDown
+	jne		_onKeyDown_NotW
 	mov		byte [wKeyDown], 1
-_notWKeyDown:
+_onKeyDown_NotW:
 	cmp		dword [ebp_wparam], 0x53	; S key
-	jne		_notSKeyDown
+	jne		_onKeyDown_NotS
 	mov		byte [sKeyDown], 1
-_notSKeyDown:
+_onKeyDown_NotS:
 	cmp		dword [ebp_wparam], 0x26	; Up Arrow
-	jne		_notUpArrowDown
+	jne		_onKeyDown_NotUpArrow
 	mov		byte [upArrowDown], 1
-_notUpArrowDown:
+_onKeyDown_NotUpArrow:
 	cmp		dword [ebp_wparam], 0x28	; Down Arrow
-	jne		_notDownArrowDown
+	jne		_onKeyDown_NotDownArrow
 	mov		byte [downArrowDown], 1
-_notDownArrowDown:
+_onKeyDown_NotDownArrow:
 	mov		esp, ebp
 	pop		ebp
 	ret		16
 
 _onKeyUp:
 	cmp		dword [ebp_wparam], 0x57	; W key
-	jne		_notWKeyUp
+	jne		_onKeyUp_NotW
 	mov		byte [wKeyDown], 0
-_notWKeyUp:
+_onKeyUp_NotW:
 	cmp		dword [ebp_wparam], 0x53	; S key
-	jne		_notSKeyUp
+	jne		_onKeyUp_NotS
 	mov		byte [sKeyDown], 0
-_notSKeyUp:
+_onKeyUp_NotS:
 	cmp		dword [ebp_wparam], 0x26	; Up Arrow
-	jne		_notUpArrowUp
+	jne		_onKeyUp_NotUpArrow
 	mov		byte [upArrowDown], 0
-_notUpArrowUp:
+_onKeyUp_NotUpArrow:
 	cmp		dword [ebp_wparam], 0x28	; Down Arrow
-	jne		_notDownArrowUp
+	jne		_onKeyUp_NotDownArrow
 	mov		byte [downArrowDown], 0
-_notDownArrowUp:
+_onKeyUp_NotDownArrow:
 	mov		esp, ebp
 	pop		ebp
 	ret		16
@@ -289,7 +415,7 @@ _onClose:
 	pop		ebp
 	ret		16
 
-quitProgram:
+_onQuit:
 	push	0
 	call	[ExitProcess]
 	ret
